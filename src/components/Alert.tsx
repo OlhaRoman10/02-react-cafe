8a6475135b9eb32ee67b896950339c8695a9b9b2
Olha_Roman <ovgrozdova79@gmail.com>
2026-06-13 21:40:21 +0200

    CafeInfo

---
+++ src/components/Alert.tsx
@@ -0,0 +1,21 @@
+// src/components/Alert.tsx
+
+import clsx from "clsx";
+import css from "./Alert.module.css";
+
+
+
+interface AlertProps {
+    type?: "success" | "error";
+    message: string;
+}
+
+export default function Alert({ type, message }:
+    AlertProps) {
+    return (
+        <p className={clsx(css.alert, type ? css[type] : "")}>
+            {message}
+        </p>
+    );
+  
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,22 @@
-// src/main.tsx
-
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./components/App/App";
+import Alert from "./components/Alert";
+import "modern-normalize";
+import "./global.css";
+// import Icon from "./components/Icon";
+
+
+
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
+    <Alert message="This is a default alert without type!" />
+    <Alert type="success" message="This is a success alert!" />
+    <Alert type="error" message="This is an error alert!" />
+    
   </React.StrictMode>
 );
+
 
